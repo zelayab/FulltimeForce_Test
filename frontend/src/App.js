49c208e3ef3a24f58c50cc8c2215c08cd5778528
zelayab <bernardo_zelaya@hotmail.com>
2023-11-14 03:55:55 -0300

    Refactor CommitComponent and CommitsListComponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { CardComponent } from './components/CardComponent/CardComponent';
 import { CommitsListComponent } from './components/CommitsListComponent/CommitsListComponent';
 
@@ -7,43 +7,43 @@ function App() {
   return (
     <Router>
       <nav class="flex items-center justify-between flex-wrap bg-blue-500 p-6">
-          <div class="flex items-center flex-shrink-0 text-white mr-6">
-            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-            <span class="font-semibold text-xl tracking-tight">FullTimeForce Test</span>
-          </div>
-          <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-            </button>
-          </div>
-          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div class="text-sm lg:flex-grow">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
+          <span class="font-semibold text-xl tracking-tight">FullTimeForce Test</span>
+        </div>
+        <div class="block lg:hidden">
+          <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          </button>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow mr-5">
             <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
               <Link to="/">Home</Link>
-              </a>
-              
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-              <Link to="/docs">Docs Github Api</Link>
-              </a>
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-              <Link to="/commits">Commits</Link>
-              </a>
-              </div>
-            <div>
-              <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-            </div>
-          </div>
-        </nav>
+            </a>
 
-        <Routes>
-          <Route path="/" element={<CardComponent 
-            img="https://avatars.githubusercontent.com/u/9919?s=280&v=4"
-            title="Github"
-            text="Github is a development platform inspired by the way you work. From open source to business, you can host and review code, manage projects, and build software alongside 50 million developers."
-          />} />
-          <Route path="/commits" element={<CommitsListComponent />} />
-          <Route path="/docs" element={<a href="https://docs.github.com/en/rest" target="_blank">Github Api</a>} />
-        </Routes>
+            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mx-4">
+              <Link to="/docs">Docs Github Api</Link>
+            </a>
+            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              <Link to="/commits">Commits</Link>
+            </a>
+          </div>
+          <div>
+            <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<CardComponent
+          img="https://avatars.githubusercontent.com/u/9919?s=280&v=4"
+          title="Github"
+          text="Github is a development platform inspired by the way you work. From open source to business, you can host and review code, manage projects, and build software alongside 50 million developers."
+        />} />
+        <Route path="/commits" element={<CommitsListComponent />} />
+        <Route path="/docs" element={<a href="https://docs.github.com/en/rest" target="_blank">Github Api</a>} />
+      </Routes>
     </Router>
   );
 }
--- frontend/src/components/CommitsListComponent/CommitsListComponent.jsx
+++ frontend/src/components/CommitsListComponent/CommitsListComponent.jsx
@@ -1,18 +1,17 @@
 import axios from 'axios';
 import React from 'react';
-import { CommitComponent } from '../CommitComponent/CommitComponent';
+import { CommitComponent } from '../CommitComponent/CommitComponent.jsx';
 
-const Commits = props=>(
-		<h4>{props.commits._id}</h4>
+const Commits = props => (
+	<>{props.commit.url.split('/')[5]}</>
 )
 
-export  const CommitsListComponent = (props) => {
+export const CommitsListComponent = (props) => {
 	const [commits, setCommits] = React.useState([]);
 
 	React.useEffect(() => {
 		axios.get('http://localhost:3001/github/commits')
 			.then(response => {
-				console.log(response.data)
 				setCommits(response.data)
 			})
 			.catch((error) => {
@@ -21,23 +20,31 @@ export  const CommitsListComponent = (props) => {
 	}, [])
 
 
-	return(
+	return (
 		<div>
 			<h1 className="text-3xl font-bold text-center text-gray-900 mb-10">
 				Commits List
 			</h1>
-			{commits.map(
-				commit => 
-					<CommitComponent 
-						name={commit.commit.author.name} 
-						message={commit.commit.message} 
-						date={commit.commit.author.date}
-						img={commit.author.avatar_url}
-						toCommit={commit.html_url}
-						key={commit.author.id}
-					/>
+			<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				Link to last Commit :
+				<a href={commits.length > 0 && commits[0].html_url} target="_blank" className="text-blue-500 hover:text-blue-800 font-bold py-2 px-4 rounded inline-flex items-center pl-4">
+					{commits.length > 0 && <Commits commit={commits[0]} />}
+				</a>
+			</button>
+			<div className="grid grid-cols-3 gap-4">
+				{commits.map(
+					commit =>
+						<CommitComponent
+							name={commit.commit.author.name}
+							message={commit.commit.message}
+							date={commit.commit.author.date}
+							img={commit.author.avatar_url}
+							toCommit={commit.html_url}
+							key={commit.author.id}
+						/>
 				)
-			}
+				}
+			</div>
 		</div>
 	)
 }
